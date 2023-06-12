386bca544cc40d78a541d0adea6bd359eccbeb2b
Attraya Das <attrayaghoshdas@gmail.com>
2023-06-12 10:18:03 +0530

    Java 12 - Unicode Characters

---
+++ java-12-features/src/com/attraya/java12/unicodechars/Chess.java
@@ -0,0 +1,25 @@
+package com.attraya.java12.unicodechars;
+
+/**
+ * New unicode characters were added. It supports a lot of Chinese language
+ * characters
+ *
+ */
+public class Chess {
+
+	public static void main(String[] args) {
+
+		System.out.println("\u265A");
+		System.out.println("\u265B");
+
+		System.out.println("\u2654");
+		System.out.println("\u2655");
+	}
+
+}
+
+//Output:
+//	♚
+//	♛
+//	♔
+//	♕
